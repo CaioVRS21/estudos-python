e7f30f32abf9f1065101e00e808c5a0f9534e6d4
CaioVRS21 <veras.caio12@gmail.com>
2022-06-25 18:31:39 -0400

    Desafio 06 corrigido

--- AulasCursoEmVideo/aulas/aula07/aula07Desafio06.py
+++ AulasCursoEmVideo/aulas/aula07/aula07Desafio06.py
@@ -2,6 +2,6 @@ num = int(input('Digite um valor: '))
 
 dobro = num * 2
 triplo = num * 3
-raiz = num * num
+raiz = num ** (1/2)
 
-print('O valor digitado foi: {}. Seu dobro é de {}, seu triplo {} e sua raiz quadrada é de {}'.format(num, dobro, triplo, raiz))
+print('O valor digitado foi: {}. Seu dobro é de {}, seu triplo {} e sua raiz quadrada é de {:.2f}'.format(num, dobro, triplo, raiz))
