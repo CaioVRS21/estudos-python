fd4c623a0c397d297a4c3843b3d9d76a8e58523b
CaioVRS21 <veras.caio12@gmail.com>
2022-06-25 18:28:19 -0400

    Teste

--- AulasCursoEmVideo/aulas/aula07/aula07Desafio05.py
+++ AulasCursoEmVideo/aulas/aula07/aula07Desafio05.py
@@ -1,4 +1,2 @@
 num = int(input('Digite um número: '))
-ant = num - 1
-suc = num + 1
-print('O antecessor de {} é {} e seu sucessor é {}'.format(num, ant, suc))
+print('O antecessor de {} é {} e seu sucessor é {}'.format(num, (num-1), (num+1)))
