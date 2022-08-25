140b9dcba07098567662b2629ca5614c75cb0916
CaioVRS21 <veras.caio12@gmail.com>
2022-08-25 16:01:32 -0400

    Exercício 75 sobre tuplas finalizado

---
+++ AulasCursoEmVideo/desafios/analise_de_dados_tupla.py
@@ -0,0 +1,16 @@
+valores = (
+int(input('Digite um valor: ')),
+int(input('Digite outro valor: ')),
+int(input('Digite mais um valor: ')),
+int(input('Digite o último valor: ')),
+)
+pares = []
+print(f'O valor 9 apareceu {valores.count(9)} vezes')
+if valores.__contains__(3):
+    print(f'O primeiro número 3 apareceu na posição {valores.index(3)}')
+else:
+    print('O número 3 não apareceu nenhuma vez')
+for valor in valores:
+    if valor % 2 == 0:
+        pares.append(valor)
+print(f'O total de números pares digitados foi {len(pares)}')
