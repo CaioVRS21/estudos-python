2b995f497b82a0774c0f4f24039d2238b311285a
CaioVRS21 <veras.caio12@gmail.com>
2022-09-12 16:35:16 -0400

    Exercício 98 sobre sobre funções completo com a solução proposta

---
+++ AulasCursoEmVideo/desafios/funcao-contador2.py
@@ -0,0 +1,23 @@
+from time import sleep
+def conta (i, f, p):
+    print('-='*30)
+    print(f'Contagem de {i} até {f} de {p} em {p}')
+    print('-='*30)
+    if i > f:
+        while i >= f:
+            sleep(1)
+            print(i, end=' ')
+            i -= p
+    elif i < f:
+        while i <= f:
+            sleep(1)
+            print(i, end=' ')
+            i += p
+    print('FIM!')
+conta(0, 10, 1)
+conta(0, 10, 2)
+print('Agora é sua vez de personalizar a contagem')
+i = int(input('Ínicio: '))
+f = int(input('Fim: '))
+p = int(input('Passo: '))
+conta(i, f, p)
