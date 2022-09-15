bec34d8b148d321342b815d5ca8a0afbb6579c84
CaioVRS21 <veras.caio12@gmail.com>
2022-09-15 17:36:34 -0400

    Exercício 104 sobre funções finalizado

---
+++ AulasCursoEmVideo/desafios/validador-de-num.py
@@ -0,0 +1,16 @@
+def leiaInt(msg):
+    valor = 0
+    ok = False
+    while True:
+        n = str(input(msg))
+        if n.isnumeric():
+            valor = int(n)
+            ok = True
+        else:
+            print('ERRO! Digite um número inteiro válido')
+        if ok:
+            break
+    return valor
+n = leiaInt('Digite um número: ')
+print(f'Você acabou de digitar o número {n}')
+
