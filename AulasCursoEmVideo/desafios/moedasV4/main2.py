9ac206e8e0c43045ee633d0086856547e83e16a1
CaioVRS21 <veras.caio12@gmail.com>
2022-09-21 18:25:44 -0400

    Exercício 110 sobre modularizção atualizado

--- AulasCursoEmVideo/desafios/moedasV4/main2.py
+++
@@ -1,3 +0,0 @@
-import funcoes2
-val = float(input('Digite um valor: R$'))
-funcoes2.resumo(val, 20, 12)
---
+++ AulasCursoEmVideo/desafios/moedasV4/main4.py
@@ -0,0 +1,3 @@
+import funcoes4
+val = float(input('Digite um valor: R$'))
+funcoes4.resumo(val, 20, 12)
